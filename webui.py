9f1aaa5fabf9e337db45cbb5ba7b2a1f90f1e6a6
YeungFurong <71367604+yangfurong09@users.noreply.github.com>
2023-07-20 11:58:11 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -357,7 +357,7 @@ def api_only():
     api = create_api(app)
 
     modules.script_callbacks.app_started_callback(None, app)
-
+    modules.script_callbacks.before_ui_callback()
     print(f"Startup time: {startup_timer.summary()}.")
     api.launch(server_name="0.0.0.0" if cmd_opts.listen else "127.0.0.1", port=cmd_opts.port if cmd_opts.port else 7861)
 
